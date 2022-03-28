4a1961fee144736998c009d5530ed521ca252e7c
توسعه‌دهنده ارشد وب مسلم شاهسون <M.Shahsavan@exir.rayaneh>
2022-03-28 09:05:33 +0430

    Add react-router-dom package and config Router routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 // import App1 from "./project1-fact/App";
 // import App2 from "./Project2-airbnb/App";
-import App3 from "./project3_meme/App";
+// import App3 from "./project3_meme/App";
 
-function App() {
+import { NavLink, Link, Outlet } from "react-router-dom";
+
+function App({ children, to, ...props }) {
+  const activeStyle = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+    };
+  };
   return (
     <div className="App">
-     {/* <App1 /> */}
-     {/* <App2 /> */}
-     <App3 />
-    </div> 
+      <nav>
+        <NavLink style={activeStyle} to="fact">
+          Fact
+        </NavLink>{" "}
+        |
+        <NavLink style={activeStyle} to="airbnb">
+          Airbnb
+        </NavLink>{" "}
+        |{" "}
+        <NavLink style={activeStyle} to="meme">
+          Meme
+        </NavLink>{" "}
+        |{" "}
+      </nav>
+      <Outlet />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Fact from "./project1-fact/App";
+import Airbnb from "./Project2-airbnb/App";
+import Meme from "./project3_meme/App";
+
+import reportWebVitals from "./reportWebVitals";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {/* <Route index element={<Fact />} /> */}
+          <Route path="fact" element={<Fact />} />
+          <Route path="airbnb" element={<Airbnb />} />
+          <Route index element={<Meme />} />
+          <Route path="meme" element={<Meme />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
