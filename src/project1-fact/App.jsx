243958858ba36ce008fe60b086c475ca9a362373
توسعه‌دهنده ارشد وب مسلم شاهسون <M.Shahsavan@exir.rayaneh>
2022-03-28 10:32:03 +0430

    Add Lazy Component sample

--- src/project1-fact/App.jsx
+++ src/project1-fact/App.jsx
@@ -1,15 +1,20 @@
 
 // Figma: https://www.figma.com/file/xA1rJVQOorqMW6xjGdBLcI/ReactFacts
 
-import Navbar from "./components/Navbar";
-import Main from "./components/Main";
+import React, { Suspense } from "react";
 
+// import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 import "./App.css";
+
+const Navbar = React.lazy(() => import("./components/Navbar"));
 
 export default function App() {
   return (
     <div className="proj1 App container">
-      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
+      </Suspense>
       <Main />
     </div>
   );
